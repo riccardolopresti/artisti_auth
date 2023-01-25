2a263c519a32c94ae322b38484c259c821c40e57
“Ibra1822” <ibrahimmujagic18@gmail.com>
2023-01-25 16:27:36 +0100

    add show artworks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             MuseumSeeder::class,
+            ArtistsTableSeeder::class,
             ArtworksTableSeeder::class,
-            ArtistsTableSeeder::class
         ]);
     }
 }
