83a0c065230179362f78ac11dcbf8d249dcd8634
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2023-01-25 16:03:00 +0100

    [fix]aside

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             MuseumSeeder::class,
-            ArtworksTableSeeder::class,
-            ArtistsTableSeeder::class
+            ArtistsTableSeeder::class,
+            ArtworksTableSeeder::class
+
         ]);
     }
 }
